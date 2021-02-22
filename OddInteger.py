a99b63e0a3284569c4b5b252febdf1b75d140ba6
Will Templeton <75176144+william01424@users.noreply.github.com>
2021-02-22 17:44:51 +0000

    Update OddInteger.py

--- OddInteger.py
+++ OddInteger.py
@@ -12,4 +12,12 @@ def find_it(seq):
     counted = dict(Counter(seq))    # Need to convert this to a dictionary so I can then access key provided value passes my condition.
     for key, value in counted.items():
         if value % 2 != 0:    # if value is odd return key
-            return key        
+            return key
+
+''' 
+Note the following solution that doesn't use Counter
+'''
+def find_it(seq):
+    for i in seq:
+        if seq.count(i) % 2 != 0:
+            return i
