03dab3c3eff70a01d1ffe9368cb9dddd470135d0
Will Templeton <75176144+william01424@users.noreply.github.com>
2021-02-22 16:36:49 +0000

    Create HashtagGenerator.py

---
+++ HashtagGenerator.py
@@ -0,0 +1,17 @@
+'''
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+'''
+def generate_hashtag(s):
+    if len(s) > 140 or s == '': return False    # Initial constraints
+    s = s.title()   # Caps every word
+    s = s.replace(" ","")   # Replaces spaces with nothing.
+    hash = '#' + s    # Add '#' to s and return hash (the final answer)
+    return hash
