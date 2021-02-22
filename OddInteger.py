4bb37aed10fd26ccd0ecfede7e79e4b1c371f19f
Will Templeton <75176144+william01424@users.noreply.github.com>
2021-02-22 17:39:48 +0000

    Create OddInteger.py

---
+++ OddInteger.py
@@ -0,0 +1,15 @@
+'''
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+'''
+
+# For this, I can import Counter from collections.
+
+from collections import Counter
+
+def find_it(seq):
+    counted = dict(Counter(seq))    # Need to convert this to a dictionary so I can then access key provided value passes my condition.
+    for key, value in counted.items():
+        if value % 2 != 0:    # if value is odd return key
+            return key        
