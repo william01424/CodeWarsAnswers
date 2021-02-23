cd3aa85306d2d7368aa0b25c6db6bca5f17a2d14
Will Templeton <75176144+william01424@users.noreply.github.com>
2021-02-23 16:22:52 +0000

    The observed PIN

---
+++ GetPins.py
@@ -0,0 +1,51 @@
+'''
+Alright, detective, one of our colleagues successfully observed our target person, Robby the robber. 
+We followed him to a secret warehouse, where we assume to find all the stolen stuff. The door to this warehouse is secured by an electronic combination lock. 
+Unfortunately our spy isn't sure about the PIN he saw, when Robby entered it.
+
+The keypad has the following layout:
+
+┌───┬───┬───┐
+│ 1 │ 2 │ 3 │
+├───┼───┼───┤
+│ 4 │ 5 │ 6 │
+├───┼───┼───┤
+│ 7 │ 8 │ 9 │
+└───┼───┼───┘
+    │ 0 │
+    └───┘
+He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually be another adjacent digit (horizontally or vertically, but not diagonally).
+E.g. instead of the 1 it could also be the 2 or 4. And instead of the 5 it could also be the 2, 4, 6 or 8.
+
+He also mentioned, he knows this kind of locks. You can enter an unlimited amount of wrong PINs, they never finally lock the system or sound the alarm. 
+That's why we can try out all possible (*) variations.
+
+* possible in sense of: the observed PIN itself and all variations considering the adjacent digits
+
+Can you help us to find all those variations? It would be nice to have a function, that returns an array (or a list in Java and C#) 
+of all variations for an observed PIN with a length of 1 to 8 digits.
+We could name the function getPINs (get_pins in python, GetPINs in C#). 
+But please note that all PINs, the observed one and also the results, must be strings, because of potentially leading '0's. 
+We already prepared some test cases for you.
+
+Detective, we are counting on you!
+'''
+
+import itertools    # allows us to iterate through all combinations of given lists of numbers
+
+def get_pins(observed):   # Creating a dictionary to give list of all the possible values when a single number is pressed.
+    PINdict = {'1': ['1','2','4'],
+               '2': ['2','1','3','5'],
+               '3': ['3','2','6'],
+               '4': ['4','1','5','7'],
+               '5': ['5','2','4','6','8'],
+               '6': ['6','3','5','9'],
+               '7': ['7','4','8'],
+               '8': ['8','5','7','9','0'],
+               '9': ['9','6','8'],
+               '0': ['0','8']
+               }
+    x = [PINdict [i] for i in observed]   # Generates a list of the PINdict.value() for each character in observed. i.e. if obs contains '1', list will include ['1', '2', '4'].
+    combinations = list(itertools.product(*x))    # Generate list of all combinations of every index in x (*x).
+    PINS = [''.join(x) for x in combinations]   # Joins strings to make one list of pins.
+    return PINS
