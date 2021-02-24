df0644c63af102303db3e8c56ddcfe9db0beddd3
Will Templeton <75176144+william01424@users.noreply.github.com>
2021-02-24 16:05:35 +0000

    Persistent Bugger.
    Can also solve this recursively, but CW did not like the declaration of a global from within the function in order to get a count - not sure why? Solved this iteratively using the above method.

---
+++ IterativeLoop.py
@@ -0,0 +1,25 @@
+'''
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
+which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example:
+
+persistence(39) => 3  # Because 3*9 = 27, 2*7 = 14, 1*4=4
+                       # and 4 has only one digit.
+
+persistence(999) => 4 # Because 9*9*9 = 729, 7*2*9 = 126,
+                       # 1*2*6 = 12, and finally 1*2 = 2.
+
+persistence(4) => 0   # Because 4 is already a one-digit number.
+'''
+
+def persistence(n):
+    count = 0
+    while n > 9:
+        count += 1
+        digits = [int(i) for i in str(n)]
+        product = 1
+        for i in digits:
+            product *= i
+        n = product
+    return count
