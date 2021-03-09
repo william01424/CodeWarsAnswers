785e43ab99b8d4772a7a3ee57dc27db1fa1e25a2
Will Templeton <75176144+william01424@users.noreply.github.com>
2021-03-09 11:13:46 +0000

    Updated with optimal method

--- CountingBits.py
+++ CountingBits.py
@@ -1,8 +1,14 @@
 '''
-Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. 
+You can guarantee that input is non-negative.
 
 Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 '''
 
 def count_bits(n):    # Key thing to note here is the bin() function.
     return str(bin(n)).count('1')
+
+# See top answer here (very similar)
+
+def countBits(n):       # Don't need to convert to a string first to use the count method.
+    return bin(n).count("1")
