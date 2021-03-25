ec5082e696f3f5da8b04eb31d287ff9a7cce36d1
Will Templeton <75176144+william01424@users.noreply.github.com>
2021-03-25 16:50:50 +0000

    Build a pile of cubes - CodeWars - 6 Kyu

--- BuildaPileofCubes.py
+++ BuildaPileofCubes.py
@@ -1,16 +1,19 @@
-# Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, 
-# the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+'''
+Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, 
+the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
 
-# You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
 
-# The parameter of the function findNb (find_nb, find-nb, findNb) will be an integer m and you have to return the integer n 
-# such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
+The parameter of the function findNb (find_nb, find-nb, findNb) will be an integer m and you have to return the integer n 
+such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
+'''
+
 
 def find_nb(m):
     n = 0
     total = 0
     while total < m:
-        n = n + 1
+        n += 1      # Different way of writing n = n + 1
         total = total + n**3
     if total == m:
         return n
