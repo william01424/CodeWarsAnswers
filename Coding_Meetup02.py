ea6a46050adbb20c3ed8c13b0f30d4e20f9e3f41
Will Templeton <75176144+william01424@users.noreply.github.com>
2021-04-16 13:11:13 +0100

    Coding Meetup #2 - 7 Kyu

---
+++ Coding_Meetup02.py
@@ -0,0 +1,20 @@
+'''
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+
+Hi < firstName here >, what do you like the most about < language here >?
+
+For example, given the following input array:
+
+list1 = [
+  { 'firstName': 'Sofia', 'lastName': 'I.', 'country': 'Argentina', 'continent': 'Americas', 'age': 35, 'language': 'Java' },
+  { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 35, 'language': 'Python' },
+  { 'firstName': 'Madison', 'lastName': 'U.', 'country': 'United States', 'continent': 'Americas', 'age': 32, 'language': 'Ruby' } 
+]
+'''
+
+def greet_developers(lst):
+    for dict in lst:
+        dict['greeting'] = 'Hi {}, what do you like the most about {}?'.format(dict['firstName'], dict['language'])   
+    return lst
